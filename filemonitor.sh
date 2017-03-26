99e40940ae17870cf7f6b3565ec63136d6eb51be
m33k <tempewillnotlast@hotmail.com>
2017-03-25 21:38:32 -0400

    Starting and adding file monitor bash script for CentOS7

---
+++ filemonitor.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Author: m33k
+# Date: 3-25-2017 -- Saturday Night Scripting
+# Description: An interactive script to monitor files and directories using auditd.
+# Version: 0.1
+
+# Start of Script
+
+
