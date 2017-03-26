39059bda42e9c0ce91ebd4f2b6352e4ccc0074ab
m33k <tempewillnotlast@hotmail.com>
2017-03-25 23:15:12 -0400

    Checks (Installed, Dead, and Failed)  written for auditd

--- filemonitor.sh
+++ filemonitor.sh
@@ -6,4 +6,21 @@
 
 # Start of Script
 
+# Variable Declarations
+auditOff=`systemctl status auditd | grep -i dead | awk '{print $3}'`
+auditFailed=`systemctl status auditd | head -3 | grep failed | awk {'print     $2'}`
 
+# Check if Audit is installed
+
+type auditd > /dev/null 2>&1 || { echo >&2 "This script requires the audit rpm to be installed. Please install it and run the script. Program Terminated..."; }
+exit 1;
+
+# Check if audit is running
+
+if [ "$auditOff" == "(dead)" ]; then
+  echo "auditd is not running."
+
+elif [ "$auditFailed" == failed ]; then
+  echo "auditd failed! Please Check Logs."
+
+fi
