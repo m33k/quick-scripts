45bcd34d75972b419c6fec585b12d4c36289ecf1
m33k <tempewillnotlast@hotmail.com>
2017-08-05 14:42:39 -0400

    It looks like I am able to commit multiples times without pushing the changes to the remote repo! Thank You Jesus. This is going to save me time.

--- test.sh
+++ test.sh
@@ -2,4 +2,6 @@
 # Created this file to see if I can commit changes without pushing to remote until I'm finished for the day.
 echo "Test"
 echo "Test2"
+echo "Test3"
 
+echo " Success! I have commited multiple times without pushing to remote repo branch"
