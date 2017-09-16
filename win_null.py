d397616a6d592372caaa4537a1609e45cd9a732a
m33k <tempewillnotlast@hotmail.com>
2017-09-16 01:45:45 -0400

    Create null dir if it doesn't exits

--- win_null.py
+++ win_null.py
@@ -3,4 +3,10 @@
 # Description: Create a /dev/null like file for windows and delete contents in it
 # Environment: For Windows 7
 
+import os
+
 # Create null file if it doesn't exists
+nullFile = ('C:\\Users\\Public\\null')
+if not os.path.isdir(nullFile):
+  os.makedirs(nullFile)
+  print("Creating win null file at" ,nullFile)
