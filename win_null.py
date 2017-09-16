532f3ab47a2a7cb12f9822fd60ababe381136b14
m33k <tempewillnotlast@hotmail.com>
2017-09-16 01:41:27 -0400

    Init

---
+++ win_null.py
@@ -0,0 +1,6 @@
+# Author: m33k
+# Date: 2017-09-16
+# Description: Create a /dev/null like file for windows and delete contents in it
+# Environment: For Windows 7
+
+# Create null file if it doesn't exists
