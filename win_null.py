6d5e41c4fa493c89241f0084f24c4935f60c6a1d
m33k <tempewillnotlast@hotmail.com>
2017-09-17 08:23:07 -0400

    Remove files in directory, leave parent

--- win_null.py
+++ win_null.py
@@ -4,9 +4,20 @@
 # Environment: For Windows 7
 
 import os
+import shutil
 
 # Create null file if it doesn't exists
 nullFile = ('C:\\Users\\Public\\null')
 if not os.path.isdir(nullFile):
   os.makedirs(nullFile)
   print("Creating win null file at" ,nullFile)
+
+# Recursively delete files, but leave parent directory.
+for file in os.listdir(nullFile):
+  filePath =  os.path.join(nullFile, file)
+  try:
+    if os.path.isfile(filePath):
+      os.unlink(filePath)
+    elif os.path.isdir(filePath): shutil.rmtree(filePath)
+  except Exception as e:
+    print(e)
