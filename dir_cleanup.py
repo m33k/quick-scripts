321b995f14e57132d831fd7133ccb6d53f14f9da
m33k <tempewillnotlast@hotmail.com>
2017-09-19 21:40:58 -0400

    Create tmp dir in Downloads

--- dir_cleanup.py
+++ dir_cleanup.py
@@ -3,4 +3,14 @@
 # Description: Create a tmp folder in downloads and delete content older than x days.
 # Environment: For Windows 7
 
+import os
+
+tmpdir = ('C:\\Users\\$USER\\Downloads\\tmp\\')
+
+# Create dir if missing
+if not os.path.exists(tmpdir):
+    os.mkdir(tmpdir)
+    print("Creating tmp dir...")
+else:
+    print("tmp dir already exists...")
 
